54429a02b7e7ee335faf3fd2fab9bf57d40ee119
Youngiyong <youn9354@naver.com>
2022-10-22 23:28:03 +0900

    fix) github action test..

---
+++ script/deploy.sh
@@ -0,0 +1,6 @@
+kill `ps -ef | grep "/opt/gofield/gofield-backend-deploy/gofield-api-user-service/build/libs/gofield-api-user-service.jar" | grep -v grep | awk '{print $2}'`
+kill `ps -ef | grep "/opt/gofield/gofield-backend-deploy/gofield-admin/build/libs/gofield-admin.jar" | grep -v grep | awk '{print $2}'`
+kill `ps -ef | grep "/opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar" | grep -v grep | awk '{print $2}'`
+nohup java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-api-user-service/build/libs/gofield-api-user-service.jar &
+nohup java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-admin/build/libs/gofield-admin.jar &
+nohup java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar &
