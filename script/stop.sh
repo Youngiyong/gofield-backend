a07642492d0c6bf8ccbaa1546795bc5735af0df0
Youngiyong <youn9354@naver.com>
2022-10-23 02:04:20 +0900

    fix) github action test..

---
+++ script/stop.sh
@@ -0,0 +1,3 @@
+sudo pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-api-user-service/build/libs/gofield-api-user-service.jar
+sudo pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-admin/build/libs/gofield-admin.jar
+sudo pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar
