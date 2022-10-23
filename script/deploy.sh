fccae83e7e0e7b95d0505b15556b4719e23fa0fd
Youngiyong <youn9354@naver.com>
2022-10-23 20:43:31 +0900

    fix) github action 적용

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,5 @@
+sudo su -
+
 pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar
 pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-admin/build/libs/gofield-admin.jar
 pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-api-user-service/build/libs/gofield-api-user-service.jar
