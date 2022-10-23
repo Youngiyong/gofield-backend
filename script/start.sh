489a3be1df41977d845ca892d80d2d537c6c19e8
Youngiyong <youn9354@naver.com>
2022-10-23 20:35:18 +0900

    fix) github action 적용

--- script/start.sh
+++
@@ -1,3 +0,0 @@
-java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-admin/build/libs/gofield-admin.jar &
-java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar &
-java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-api-user-service/build/libs/gofield-api-user-service.jar &
