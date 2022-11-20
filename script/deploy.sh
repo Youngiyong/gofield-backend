f416b8ce4d3f1322735e063444ccf1a7ce8275d8
Youngiyong <youn9354@naver.com>
2022-11-20 21:23:26 +0900

    fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,3 @@
-echo 'hi'
-
 pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar
 pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-admin/build/libs/gofield-admin.jar
 pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-api-user-service/build/libs/gofield-api-user-service.jar
@@ -8,5 +6,4 @@ java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofie
 java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar &
 java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-api-user-service/build/libs/gofield-api-user-service.jar &
 
-echo 'end'
 exit 0
