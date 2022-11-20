d46130081703af7b2bcde97fecd6e707d956e644
Youngiyong <youn9354@naver.com>
2022-11-20 20:53:42 +0900

    fix) test

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,5 +8,4 @@ java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofie
 java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar &
 java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-api-user-service/build/libs/gofield-api-user-service.jar &
 
-exit 1
-exit 1
+exit 0
