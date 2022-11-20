9653035185449d537de70af98c0036b4693dce63
Youngiyong <youn9354@naver.com>
2022-11-20 20:03:17 +0900

    fix) test

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,3 @@
 sudo pkill -9 -f /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar
 
-sudo nohup java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar
+sudo nohup java -jar -Dspring.profiles.active=dev /opt/gofield/gofield-backend-deploy/gofield-sns/build/libs/gofield-sns.jar &
