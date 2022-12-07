2c5a795de3cc1adad2bd38448e47773004a6781f
Youngiyong <youn9354@naver.com>
2022-12-08 00:18:55 +0900

    fix s3 -> cloudfront url로 변경

--- gofield-common/src/main/java/com/gofield/common/model/Constants.java
+++ gofield-common/src/main/java/com/gofield/common/model/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String BASE_PACKAGE = "com.gofield";
     public static final String TOKEN_ISSUER = "service.gofield.shop";
 
-//    public static final String CDN_URL = "https://d22b51gh5o1q1e.cloudfront.net";
-    public static final String CDN_URL = "https://gofield.s3.ap-northeast-2.amazonaws.com";
+    public static final String CDN_URL = "https://d22b51gh5o1q1e.cloudfront.net";
+//    public static final String CDN_URL = "https://gofield.s3.ap-northeast-2.amazonaws.com";
     public static final String TRACKER_DELIVERY_URL = "https://tracker.delivery/#/%s/%s";
 }
