94c4a7b60b53bd8f27ed9779eaf800e8597523b6
Youngiyong <youn9354@naver.com>
2023-01-19 00:27:00 +0900

    reviewScore type 변경

--- gofield-api-user-service/src/main/java/com/gofield/api/dto/res/ItemBundleResponse.java
+++ gofield-api-user-service/src/main/java/com/gofield/api/dto/res/ItemBundleResponse.java
@@ -18,7 +18,7 @@ public class ItemBundleResponse {
     private String brandName;
     private String thumbnail;
     private int reviewCount;
-    private String reviewScore;
+    private Double reviewScore;
     private int newLowestPrice;
     private int usedLowestPrice;
     private int allItemCount;
@@ -27,7 +27,7 @@ public class ItemBundleResponse {
     private List<String> images;
 
     @Builder
-    private ItemBundleResponse(Long id, String name, String brandName, String thumbnail, int reviewCount, String reviewScore, int newLowestPrice, int usedLowestPrice, int allItemCount,  Long newItemCount, Long usedItemCount, List<String> images){
+    private ItemBundleResponse(Long id, String name, String brandName, String thumbnail, int reviewCount, Double reviewScore, int newLowestPrice, int usedLowestPrice, int allItemCount,  Long newItemCount, Long usedItemCount, List<String> images){
         this.id = id;
         this.name = name;
         this.brandName = brandName;
@@ -49,7 +49,7 @@ public class ItemBundleResponse {
                 .brandName(projection.getBrandName())
                 .thumbnail(CommonUtils.makeCloudFrontUrl(projection.getThumbnail()))
                 .reviewCount(projection.getReviewCount())
-                .reviewScore(String.format("%.1f", projection.getReviewScore()))
+                .reviewScore(Double.parseDouble(String.format("%.1f", projection.getReviewScore())))
                 .newLowestPrice(projection.getNewLowestPrice())
                 .usedLowestPrice(projection.getUsedLowestPrice())
                 .allItemCount(projection.getAllItemCount())
