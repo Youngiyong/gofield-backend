38c658d2336b9c316bbec0767ca4522c1c0aabdc
Youngiyong <youn9354@naver.com>
2023-02-07 21:59:33 +0900

    fix

--- gofield-api-user-service/src/main/java/com/gofield/api/config/enummapper/EnumMapperFactoryConfig.java
+++ gofield-api-user-service/src/main/java/com/gofield/api/config/enummapper/EnumMapperFactoryConfig.java
@@ -22,6 +22,7 @@ public class EnumMapperFactoryConfig {
         enumMapperFactory.putEnumCodeModel("ORDER_ITEM_STATUS", EOrderItemStatusFlag.class);
         enumMapperFactory.putEnumCodeModel("ORDER_CANCEL_STATUS", EOrderCancelStatusFlag.class);
         enumMapperFactory.putEnumCodeModel("ORDER_CANCEL_TYPE", EOrderCancelTypeFlag.class);
+        enumMapperFactory.putEnumModel("PAYMENT_TYPE", EPaymentType.class);
         enumMapperFactory.putEnumModel("ORDER_CANCEL_REASON", EOrderCancelClientReason.class);
         enumMapperFactory.putEnumCodeModel("ORDER_CANCEL_ALL_REASON", EOrderCancelReasonFlag.class);
         enumMapperFactory.putEnumModel("ORDER_CHANGE_REASON", EOrderChangeClientReason.class);
