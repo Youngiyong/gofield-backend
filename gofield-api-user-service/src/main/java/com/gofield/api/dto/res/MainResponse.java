33bb8d69abdd8c40d81fa3cfbac48f8b92298d93
Youngiyong <youn9354@naver.com>
2023-02-21 22:39:52 +0900

    fix

--- gofield-api-user-service/src/main/java/com/gofield/api/dto/res/MainResponse.java
+++ gofield-api-user-service/src/main/java/com/gofield/api/dto/res/MainResponse.java
@@ -13,22 +13,19 @@ public class MainResponse {
     private List<ItemBundlePopularResponse> popularBundleList;
     private List<ItemBundleRecommendResponse> recommendBundleList;
     private List<ItemBundleRecommendResponse> categoryBundleList;
-    private List<ItemClassificationResponse> classificationItemList;
 
     @Builder
-    private MainResponse(List<ItemBundlePopularResponse> popularBundleList, List<ItemBundleRecommendResponse> recommendBundleList, List<ItemBundleRecommendResponse> categoryBundleList, List<ItemClassificationResponse> classificationItemList){
+    private MainResponse(List<ItemBundlePopularResponse> popularBundleList, List<ItemBundleRecommendResponse> recommendBundleList, List<ItemBundleRecommendResponse> categoryBundleList){
         this.popularBundleList = popularBundleList;
         this.recommendBundleList = recommendBundleList;
         this.categoryBundleList = categoryBundleList;
-        this.classificationItemList = classificationItemList;
     }
 
-    public static MainResponse of(List<ItemBundlePopularResponse> popularBundleList, List<ItemBundleRecommendResponse> recommendBundleList, List<ItemBundleRecommendResponse> categoryBundleList, List<ItemClassificationResponse> classificationItemList){
+    public static MainResponse of(List<ItemBundlePopularResponse> popularBundleList, List<ItemBundleRecommendResponse> recommendBundleList, List<ItemBundleRecommendResponse> categoryBundleList){
         return MainResponse.builder()
                 .popularBundleList(popularBundleList)
                 .recommendBundleList(recommendBundleList)
                 .categoryBundleList(categoryBundleList)
-                .classificationItemList(classificationItemList)
                 .build();
     }
 }
