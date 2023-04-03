f5593c7db7024562d2ceb0f23a9efe59ebeb41b6
Youngiyong <youn9354@naver.com>
2023-04-03 22:06:28 +0900

    [FIX]

--- gofield-core/gofield-common/src/main/java/com/gofield/common/model/Constants.java
+++ gofield-core/gofield-common/src/main/java/com/gofield/common/model/Constants.java
@@ -3,12 +3,12 @@ package com.gofield.common.model;
 public class Constants {
     public static final String METHOD = "CARD";
     public static final String BASE_PACKAGE = "com.gofield";
-    public static final String TOKEN_ISSUER = "service.gofield.shop";
+    public static final String TOKEN_ISSUER = "www.readys.co.kr";
     public static final String CDN_URL = "https://d22b51gh5o1q1e.cloudfront.net/";
     public static final String RESIZE_ADMIN = "?s=180x180&q=80&f=webp&t=crop";
     public static final String TRACKER_DELIVERY_URL = "https://tracker.delivery/#/%s/%s";
     public static final String AUTHORIZATION = "Authorization";
-    public static final String AUTH_PREFIX = "Gofield";
+    public static final String AUTH_PREFIX = "Readys";
     public static final String USER_ID = "userId";
     public static final double SAFE_PAYMENT_CHARGE = 2;
 }
